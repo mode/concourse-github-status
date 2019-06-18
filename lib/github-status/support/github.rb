f3c00f3b727282d4733bc89f1358988d47bc553d
Jason Y <jasonarewhy@users.noreply.github.com>
2019-06-18 08:10:02 -0700

    GitHub api wait limit (#2)
    * api_wait_limit
    
    * add api_wait_buffer
    
    * update README

--- lib/github-status/support/github.rb
+++ lib/github-status/support/github.rb
@@ -13,6 +13,25 @@ module GitHubStatus
         @github ||= Octokit::Client.new access_token: access_token
       end
 
+      Contract None => Bool
+      def github_ratelimit_ok?
+        rate_limit_remaining = github.rate_limit.remaining()
+        rate_limit_resets_in = github.rate_limit.resets_in()
+        
+        if rate_limit_remaining < api_wait_buffer
+          if rate_limit_resets_in < api_wait_limit
+            STDERR.puts("Github API tries remaining (#{rate_limit_remaining}) is less than buffer (#{api_wait_buffer}). Sleeping for #{rate_limit_resets_in}s..."
+            sleep rate_limit_resets_in
+            return true
+          else
+            raise "Github API rate exceeded: time to reset (#{rate_limit_resets_in}s) exceeds wait limit (#{api_wait_limit}s)"
+          end
+        else
+          # continue if we're not reaching ratelimit
+          return true
+        end
+      end
+
       Contract None => String
       def canonical_sha
         @canonical_sha ||= (sha.match(/^.{40}$/) || github.commit(repo, sha).sha).to_s
--- lib/github-status/support/source.rb
+++ lib/github-status/support/source.rb
@@ -26,6 +26,16 @@ module GitHubStatus
       def branch
         @branch ||= source.fetch('branch') { 'master' }
       end
+
+      Contract None => Int
+      def api_wait_limit
+        @api_wait_limit ||= params.fetch 'api_wait_limit', 900 # 15min
+      end
+
+      Contract None => Int
+      def api_wait_buffer
+        @api_wait_buffer ||= params.fetch 'api_wait_buffer', 60
+      end
     end
   end
 end
